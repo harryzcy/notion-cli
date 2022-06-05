a317d9782074c6183a16eba7c24b24ba46d709a2
Chongyi Zheng <harry@harryzheng.com>
2022-06-04 23:32:14 -0400

    Implement database page deletion

--- internal/api/db_delete.go
+++ internal/api/db_delete.go
@@ -1,5 +1,14 @@
 package api
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/harryzcy/notion-cli/internal/oauth2"
+	"github.com/jomei/notionapi"
+)
+
 type DatabasePageDeleteInput struct {
 	Database string
 	PageID   string
@@ -7,6 +16,41 @@ type DatabasePageDeleteInput struct {
 
 // DeletePages deletes a page from a database
 func (db database) DeletePage(input DatabasePageDeleteInput) error {
-	// TODO: implement
+	token, err := oauth2.GetToken()
+	if err != nil {
+		return fmt.Errorf("token not found, please run `notion auth` first")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client := notionapi.NewClient(notionapi.Token(token.AccessToken))
+
+	id, err := db.getID(ctx, client, input.Database)
+	if err != nil {
+		return err
+	}
+
+	page, err := client.Page.Get(ctx, notionapi.PageID(input.PageID))
+	if err != nil {
+		return err
+	}
+
+	if page.Parent.Type != "database_id" || page.Parent.DatabaseID != id {
+		return fmt.Errorf("page not found")
+	}
+
+	if page.Archived {
+		return fmt.Errorf("page is already archived")
+	}
+
+	_, err = client.Page.Update(ctx, notionapi.PageID(input.PageID), &notionapi.PageUpdateRequest{
+		Properties: notionapi.Properties{},
+		Archived:   true,
+	})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("page archived")
 	return nil
 }
