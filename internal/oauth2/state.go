cad9d81e0f0739486842a56277ebadcab2ea524f
harryzcy <harry@harryzheng.com>
2023-02-05 18:37:06 -0500

    Remove rand.Seed per go 1.20 update

--- internal/oauth2/state.go
+++ internal/oauth2/state.go
@@ -2,7 +2,6 @@ package oauth2
 
 import (
 	"math/rand"
-	"time"
 )
 
 const stateLength = 5
@@ -10,7 +9,6 @@ const stateLength = 5
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func generateRandomState() string {
-	rand.Seed(time.Now().UnixNano())
 	b := make([]rune, stateLength)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
